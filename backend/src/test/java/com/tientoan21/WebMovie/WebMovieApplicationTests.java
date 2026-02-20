07bde75baaf402dff27e29cb73b10c45bc805b31
Hoàng Tiến Toàn <toan01022005@gmail.com>
2026-02-20 21:14:14 +0700

    Init backend and frontend

---
+++ backend/src/test/java/com/tientoan21/WebMovie/WebMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientoan21.WebMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
